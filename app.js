b43a2e589fba924b8384e24c47a635559ef6adf1
Peter Nordman <petermnordman@gmail.com>
2019-12-06 14:23:10 -0500

    functionality fully working

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 const heroNames = ["Anti-Mage","Axe","Bane","Bloodseeker","Crystal Maiden","Drow Ranger","Earthshaker","Juggernaut","Mirana","Morphling","Shadow Fiend","Phantom Lancer","Puck","Pudge","Razor","Sand King","Storm Spirit","Sven","Tiny","Vengeful Spirit","Windranger","Zeus","Kunkka","Lina","Lion","Shadow Shaman","Slardar","Tidehunter","Witch Doctor","Lich","Riki","Enigma","Tinker","Sniper","Necrophos","Warlock","Beastmaster","Queen of Pain","Venomancer","Faceless Void","Wraith King","Death Prophet","Phantom Assassin","Pugna","Templar Assassin","Viper","Luna","Dragon Knight","Dazzle","Clockwerk","Leshrac","Nature's Prophet","Lifestealer","Dark Seer","Clinkz","Omniknight","Enchantress","Huskar","Night Stalker","Broodmother","Bounty Hunter","Weaver","Jakiro","Batrider","Chen","Spectre","Ancient Apparition","Doom","Ursa","Spirit Breaker","Gyrocopter","Alchemist","Invoker","Silencer","Outworld Devourer","Lycan","Brewmaster","Shadow Demon","Lone Druid","Chaos Knight","Meepo","Treant Protector","Ogre Magi","Undying","Rubick","Disruptor","Nyx Assassin","Naga Siren","Keeper of the Light","Io","Visage","Slark","Medusa","Troll Warlord","Centaur Warrunner","Magnus","Timbersaw","Bristleback","Tusk","Skywrath Mage","Abaddon","Elder Titan","Legion Commander","Techies","Ember Spirit","Earth Spirit","Underlord","Terrorblade","Phoenix","Oracle","Winter Wyvern","Arc Warden","Monkey King","Dark Willow","Pangolier","Grimstroke","Void Spirit","Snapfire","Mars"];
 
 const gameModes = ["Unknown", "All Pick", "Captains Mode", "Random Draft", "Single Draft", "All Random", "Intro", "Diretide", "Reverse Captains Mode", "The Greeviling", "Tutorial", "Mid Only", "Least Played", "Limited Heroes", "Compendium", "Custom", "Captains Draft", "Balanced Draft", "Ability Draft", "Event", "All Random Deathmatch", "1v1 Solo Mid", "All Draft", "Turbo", "Mutation"]
-/* ---------------------------- Fetch player list --------------------------- */
 
+/* ---------------------------- Fetch player list --------------------------- */
 function getPlayerList (query) {
     query = encodeURIComponent(query);
 
@@ -44,6 +44,7 @@ function getPlayerData(accountID) {
     .catch(error => console.log(error))
 }
 
+
 /* ------------------------- Fetch recent matches data ------------------------ */
 function getPlayerRecentMatches (accountID) {
     fetch(`https://api.opendota.com/api/players/${accountID}/recentMatches`)
@@ -60,21 +61,7 @@ function getPlayerRecentMatches (accountID) {
     .catch(error => console.log(error));
 }
 
-/* ------------------------- Fetch player hero data ------------------------- */
-function getPlayerHeroes (accountID) {
 
-    fetch(`https://api.opendota.com/api/players/${accountID}/heroes`)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error(response.json());
-        }
-    })
-    .then(responseJSON => {
-        populatePlayerHeroes(responseJSON, accountID);
-    })
-    .catch(error => console.log(error));
 
 }
 
